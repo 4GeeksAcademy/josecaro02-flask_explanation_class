65f347d5b7d7f08cad63110254fbe045e05eba62
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2023-08-10 02:56:19 +0000

    [Add new table planet and endpoints] -src/admin.py: Add planet model to admin -src/app.py: Create get and post users, create get, post, put and delete endpoints for planets -src/models: Create model for planet

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,76 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
+@app.route('/user/<int:user_id>', methods=['GET'])
+def handle_hello(user_id):
+    users = User.query.all()
+    single_user = User.query.get(user_id)
+    filter_users = User.query.filter_by(is_active = True)
+    # print(type(filter_users))
+    # print(filter_users)
+    filter_users_serialized = list(map(lambda x : x.serialize(), filter_users))
+    print(filter_users_serialized)
+    if single_user is None:
+        # return jsonify({"msg": f"El usuario con el id {user_id} no existe"}), 400
+        raise APIException(f"No existe el id {user_id}", status_code=400)
+    # print(single_user)
+    # print(single_user.serialize())
+    users_serialized = list(map(lambda x : x.serialize(), users))
+    # print(users_serialized)
+    # print(users)
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Hello, this is your GET /user response ",
+        "users": users_serialized,
+        "user_id": user_id,
+        "user_info": single_user.serialize()
     }
 
     return jsonify(response_body), 200
+
+@app.route('/user', methods=['POST'])
+def post_user():
+    body = request.get_json(silent=True)
+    if body is None:
+        raise APIException("Debes enviar informacion en el body", status_code=400)
+    if "email" not in body:
+        raise APIException("Debes enviar el campo email", status_code=400)
+    if "password" not in body:
+        raise APIException("Debes enviar tu contraseña", status_code=400)
+    new_user = User(email = body['email'], password = body['password'], is_active = True)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg": "Completado", "new_user_info": new_user.serialize()})
+
+
+@app.route("/planets", methods=["GET"])
+def get_planets():
+    planets = Planet.query.all()
+    planets_serialized = list(map(lambda x: x.serialize(), planets))
+    return jsonify({"msg": 'Completed', "planets": planets_serialized})
+
+@app.route("/planets", methods=['PUT'])
+def modify_planet():
+    body = request.get_json(silent=True)
+    if body is None:
+        raise APIException("Debes enviar informacion en el body", status_code=400)
+    if "id" not in body:
+        raise APIException("Debes enviar el id del planeta a modificar", status_code=400)
+    if "name" not in body:
+        raise APIException("Debes enviar el nuevo nombre del planeta", status_code=400)
+    single_planet = Planet.query.get(body['id'])
+    single_planet.name = body['name']
+    db.session.commit()
+    return jsonify({"msg": "Completed"})
+
+@app.route("/planets/<int:planet_id>", methods=['DELETE'])
+def delete_planet(planet_id):
+    single_planet = Planet.query.get(planet_id)
+    if single_planet is None:
+        raise APIException("El planeta no existe", status_code=400)
+    db.session.delete(single_planet)
+    db.session.commit()
+
+    return jsonify({"msg": "Completed"})
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,11 +9,26 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f"User with email {self.email} and id {self.id} "
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
+        }
+    
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f"El planeta {self.name} con ID: {self.id}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
         }
